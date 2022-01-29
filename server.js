5427ef94d04381438c11937cb5007d9f0feac8c9
Sidharth <sidharth.edupuganti@gmail.com>
2022-01-29 16:19:59 -0600

    set up yelp fusion api

--- server.js
+++ server.js
@@ -1,11 +1,39 @@
+require('dotenv').config();
+const { response } = require('express');
 const express = require('express');
 const app = express();
 
+
+
 const PORT = process.env.PORT|| 3000;
+const yelp = require("yelp-fusion");
+// const yelpKey = process.env.YELP_API;
+const yelpClient = yelp.client(process.env.YELP_API);
+
+
+
+const searchRequest = {
+    term: 'Tourist Attractions',
+    location: 'London, UK'
+};
+
+yelpClient.search(searchRequest).then(response => {
+    const firstResult = response.jsonBody.businesses[2];
+    const prettyJson = JSON.stringify(firstResult, null, 4);
+    console.log(prettyJson);
+  }).catch(e => {
+    console.log(e);
+  });
+
 
 app.get('/', (req, res)=> {
+
+
     res.send("Testing hello!!!!!");
+
 });
+
+
 
 app.listen(PORT, ()=> {
     console.log(`Server running on port ${PORT}`);
